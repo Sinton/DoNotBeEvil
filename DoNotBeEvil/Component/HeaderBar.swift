1d164c57a26acb65d4ce4b172dc0f83f4e1e7d5e
Sinton <yansuntong@gmail.com>
2023-04-05 11:57:30 +0800

    add HeaderBar component view

---
+++ DoNotBeEvil/Component/HeaderBar.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderBar.swift
+//  DoNotBeEvil
+//
+//  Created by Yan on 2023/3/31.
+//  Copyright © 2023 Yan. All rights reserved.
+//
+
+import SwiftUI
+
+struct HeaderBar: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HeaderBar_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderBar()
+    }
+}
