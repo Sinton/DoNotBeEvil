1c21ebffc8bbf42e2e193d35c054f8b24b402256
Sinton <yansuntong@gmail.com>
2023-04-05 11:57:01 +0800

    add settings view

---
+++ DoNotBeEvil/View/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  DoNotBeEvil
+//
+//  Created by Yan on 2023/3/31.
+//  Copyright © 2023 Yan. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
